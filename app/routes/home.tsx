4ace8787fb8040e97ea3b9fb6d26eef730a0a3ad
udit jain <uditj66@gmail.com>
2025-05-24 18:13:08 +0530

    project scafold and add setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+const Home = () => {
+  return <div>Home</div>;
+};
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
-export default function Home() {
-  return <Welcome />;
-}
+export default Home;
